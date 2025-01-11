b430a8338df303056e5d11416360354a2e2dc990
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2025-01-11 05:43:16 +0200

    split code

---
+++ src/commands/inline-query.ts
@@ -0,0 +1,238 @@
+import {fmt, link} from 'npm:@grammyjs/parse-mode'
+import {Composer, InlineKeyboard, InlineQueryResultBuilder} from 'npm:grammy'
+import {createCachedFetch, danbooruTagsBuilder} from '../deps.ts'
+import {danbooruApi, DanbooruPost, danbooruUri} from '../lib/danbooru/danbooru.ts'
+
+const bot = new Composer()
+export {bot as inline_query}
+
+const only = [
+  'id',
+  'file_ext',
+  'file_size',
+  'file_url',
+  'large_file_url',
+  'preview_file_url',
+  'tag_string_artist',
+  'tag_string_character',
+].join(',')
+
+const fetchDanbooru = await createCachedFetch({
+  name: 'danbooru',
+  ttl: 60 * 30, // 30 min
+  log: true,
+})
+
+const postToInlineResult = (post: DanbooruPost) => {
+  const uri =
+    post.file_size >= 5242880 // 5 MiB tg limit
+      ? post.large_file_url
+      : post?.file_url!
+
+  const postLink = fmt`${link(
+    post.tag_string_artist ? post.tag_string_artist : post.id,
+    new URL(`/posts/${post.id}`, danbooruUri).toString()
+  )}`
+
+  const characters = post.tag_string_character.split(' ').map((char) => {
+    const uri = new URL('/posts', danbooruUri)
+    uri.searchParams.set('tags', char)
+    return fmt` ${link(char, uri.toString())}`
+  })
+
+  const {text: caption, entities: caption_entities} = fmt([fmt`${postLink}`, ...characters])
+
+  // TODO: test video/webm/mp4
+  if (post.file_ext === 'webm') {
+    // return InlineQueryResultBuilder.v(`post-${post.id}`, uri, post.preview_file_url, {
+    // thumbnail_mime_type: 'image/jpeg',
+    // caption,
+    // caption_entities,
+    // title: 'Title'
+    // })
+  }
+
+  if (post.file_ext === 'gif') {
+    return InlineQueryResultBuilder.gif(`post-${post.id}`, uri, post.preview_file_url, {
+      thumbnail_mime_type: 'image/jpeg',
+      caption,
+      caption_entities,
+      title: 'Title',
+    })
+  }
+
+  if (post.file_ext === 'jpg' || post.file_ext === 'png') {
+    return InlineQueryResultBuilder.photo(`post-${post.id}`, uri, {
+      thumbnail_url: post.preview_file_url,
+      caption,
+      caption_entities,
+    })
+  }
+
+  // unknown post format
+  return InlineQueryResultBuilder.article(`post-${post.id}`, `/posts/${post.id}`, {
+    url: postLink.text,
+    description: `ext:${post.file_ext}`,
+  }).text(`posts/${post.id}`, {})
+}
+
+// @bot id 1 2 123
+bot.inlineQuery(/^id\s+(?<numbers>(?:\d+\s*){1,10})/, async (c, next) => {
+  const match = c.match as RegExpMatchArray
+  if (match && match.groups) {
+    const ids = match.groups.numbers.split(/\s+/).map(Number)
+    const posts = await Promise.all(
+      ids.map(async (id) => {
+        const post = await danbooruApi
+          .GET('/posts/{id}.json', {
+            params: {path: {id}},
+            fetch: fetchDanbooru,
+          })
+          .then((v) => v.data)
+
+        if (post) return postToInlineResult(post)
+      })
+    )
+
+    return await c.answerInlineQuery(posts.filter(Boolean) as [], {
+      is_personal: true,
+    })
+  }
+
+  await next()
+})
+
+// bot.inlineQuery(/^(u|user)\s+(\w+)/i, async (c, next) => {
+//   const match = c.match as RegExpMatchArray
+//   const username = match[2]
+
+//   if (username) {
+//     const {data} = await danbooruApi.GET('/autocomplete.json', {
+//       params: {
+//         query: {
+//           'search[type]': 'user',
+//           'search[query]': username,
+//           limit: 5,
+//         },
+//       },
+//     })
+
+//     console.log(data)
+//     if (data) {
+//       const items = data.map((v) => {
+//         if (v.type !== 'user') return
+//         return InlineQueryResultBuilder.article(`user:${v.id}`, v.label).text(`${v.label}`)
+//       })!
+
+//       return c.answerInlineQuery(items as any, {
+//         cache_time: 30,
+//         is_personal: true,
+//       })
+//     }
+//   }
+//   await next()
+// })
+
+// @bot fav
+// @bot favorite[s]
+bot.inlineQuery(/^fav(orite.?)?$/i, async (c, next) => {
+  const offset = parseInt(c.inlineQuery.offset) || 1
+  const {data: posts} = await danbooruApi.GET('/posts.json', {
+    fetch: fetchDanbooru,
+    params: {
+      query: {
+        tags: danbooruTagsBuilder() //
+          .ordfav('maks11060')
+          .toString(),
+        limit: 50,
+        page: offset,
+        only,
+      },
+    },
+  })
+
+  if (!posts) return await next()
+
+  const items = posts?.map(postToInlineResult)
+
+  return await c.answerInlineQuery(items, {
+    next_offset: `${offset + 1}`,
+  })
+})
+
+bot.inlineQuery(/hot/i, async (c, next) => {
+  const offset = parseInt(c.inlineQuery.offset) || 1
+  const {data: posts} = await danbooruApi.GET('/posts.json', {
+    fetch: fetchDanbooru,
+    params: {
+      query: {
+        tags: danbooruTagsBuilder() //
+          .order('rank')
+          .rating(['e', 'q'], true)
+          .tag('parent:none')
+          .toString(),
+        limit: 50,
+        page: offset,
+        only,
+      },
+    },
+  })
+
+  if (!posts) return await next()
+
+  const items = posts?.map(postToInlineResult)
+
+  return await c.answerInlineQuery(items, {
+    cache_time: 30,
+    next_offset: `${offset + 1}`,
+  })
+})
+
+// @bot s
+// @bot saves
+bot.inlineQuery(/^s(ave[s|d]?)?$/i, async (c, next) => {
+  const offset = parseInt(c.inlineQuery.offset) || 1
+  const {data: posts} = await danbooruApi.GET('/posts.json', {
+    fetch: fetchDanbooru,
+    params: {
+      query: {
+        tags: danbooruTagsBuilder() //
+          .tag('search:all')
+          .tag('parent:none')
+          .toString(),
+        limit: 50,
+        page: offset,
+        only,
+      },
+    },
+  })
+
+  if (!posts) return await next()
+
+  const items = posts?.map(postToInlineResult)
+
+  return await c.answerInlineQuery(items, {
+    cache_time: 30,
+    next_offset: `${offset + 1}`,
+  })
+})
+
+bot.on('inline_query', async (c) => {
+  console.log('%cUnknown inline query', 'color: orange', c.inlineQuery.query)
+
+  return await c.answerInlineQuery(
+    [
+      InlineQueryResultBuilder.article('t:help', 'Search help', {
+        reply_markup: new InlineKeyboard() //
+          .switchInlineCurrent('Hot', 'hot')
+          .switchInlineCurrent('Fav', 'fav')
+          .switchInlineCurrent('Saved', 'saved')
+          .toTransposed(),
+      }).text('Search help'),
+    ],
+    {
+      is_personal: true,
+      cache_time: 15,
+    }
+  )
+})
